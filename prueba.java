a22f52843c704fd4bdfcc4533ce607b9b8c03296
ViceentBertet <149675788+ViceentBertet@users.noreply.github.com>
2023-12-05 09:23:01 +0100

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -2,5 +2,11 @@ public class prueba {
     public static void main(String[] args){
         System.out.println("Hola caracola");
         System.out.println("Vicent es feo");
+        boolean Alex = true;
+        if (Alex) {
+            System.out.println("Soy mas feo que un pie");
+        } else {
+            System.out.println("Soy Guapo");
+        }
     }
 }
